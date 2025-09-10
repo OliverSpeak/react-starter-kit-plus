411703c859f8bba4c27f2c67bcf2bcceb1134243
Oliver Speak <102995517+OliverSpeak@users.noreply.github.com>
2025-09-10 14:45:51 +0100

    refactor: improve wording on welcome page

--- resources/js/pages/welcome.tsx
+++ resources/js/pages/welcome.tsx
@@ -44,7 +44,7 @@ export default function Welcome() {
                         <div className="flex-1 rounded-br-lg rounded-bl-lg bg-white p-6 pb-12 text-[13px] leading-[20px] shadow-[inset_0px_0px_0px_1px_rgba(26,26,0,0.16)] lg:rounded-tl-lg lg:rounded-br-none lg:p-20 dark:bg-[#161615] dark:text-[#EDEDEC] dark:shadow-[inset_0px_0px_0px_1px_#fffaed2d]">
                             <h1 className="mb-1 font-medium">React Starter Kit Plus</h1>
                             <p className="mb-2 text-[#706f6c] dark:text-[#A1A09A]">
-                                This is a fork of the official Laravel + React Starter Kit branch with extra boilerplate code for my own needs.
+                                This is a fork of the official React Starter Kit branch with extra boilerplate specific to my own needs.
                             </p>
                             <ul className="mb-4 flex flex-col lg:mb-6">
                                 <li className="relative flex items-center gap-4 py-2 before:absolute before:top-1/2 before:bottom-0 before:left-[0.4rem] before:border-l before:border-[#e3e3e0] dark:before:border-[#3E3E3A]">
