3af6aad1774f74172f2e0bb3cd11ebe62e2adf29
Oliver Speak <102995517+OliverSpeak@users.noreply.github.com>
2025-09-11 16:18:33 +0100

    feat: add conditional default password validation based on environment

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Validation\Rules\Password;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +20,17 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        /*
+         * Default password rules
+         *
+         * This is setup so that rules may be relaxed in non-production environments.
+         */
+        Password::defaults(function () {
+            $rule = Password::min(8);
+
+            return $this->app->environment('production')
+                ? $rule // ->uncompromised()
+                : $rule;
+        });
     }
 }
